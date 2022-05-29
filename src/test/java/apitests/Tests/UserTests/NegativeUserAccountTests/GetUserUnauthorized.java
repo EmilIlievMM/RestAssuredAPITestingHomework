0bedd1d91e38e2671a2e86ef3a98bd744a8d6c30
Emil Iliev <emil.iliev@mentormate.com>
2022-05-29 13:56:21 +0300

    Initial commit

---
+++ src/test/java/apitests/Tests/UserTests/NegativeUserAccountTests/GetUserUnauthorized.java
@@ -0,0 +1,18 @@
+package apitests.Tests.UserTests.NegativeUserAccountTests;
+
+import apitests.Setup.BaseSetup;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetUserUnauthorized extends BaseSetup {
+    @Test
+    public void getUser(){
+        given()
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/users/" + userId)
+                .then()
+                .statusCode(401);
+    }
+}
